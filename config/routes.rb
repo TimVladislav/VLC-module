cbb2c9837b8981e207495afa5e246301b473074a
TimVladislav <tiprog@rambler.ru>
2016-10-13 23:31:15 +0300

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :startpage, only: [:index]
-  resources :devices, only: [:index, :new, :show, :destroy]
+  resources :devices, only: [:index, :new, :show, :destroy, :create]
   root "startpage#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
